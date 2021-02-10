bf9264c99e28f7c4c4fde6ba69a6d26df56df113
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-02-10 13:53:09 +0100

    Display every letter in  alphabet

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-console.log('We are connected!');
+const azbuka = 'АБВГДЂЕЖЗИЈКЛЉМНЊОПРСТЋУФХЦЧЏШ';
+const azbukaArr = azbuka.split('');
 
-let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-console.log(alphabet.split(''));
+const letter = document.querySelector('h1');
+let stop = false;
+
+for (let i = 0; i < azbukaArr.length; i++) {}
+
+let character = 0;
+let timer = setInterval(onTick, 100);
+
+function onTick() {
+    letter.innerHTML = azbukaArr[character];
+    character++;
+    if (character === azbukaArr.length) {
+        character = 0;
+    }
+}
